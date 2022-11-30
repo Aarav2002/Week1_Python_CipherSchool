db836dd13fda50f29cb965710bdf812626f25ef8
Aarav2002 <108512766+Aarav2002@users.noreply.github.com>
2022-12-01 03:12:02 +0530

    Add files via upload

---
+++ class10-escape_sequences-CipherSchools.py
@@ -0,0 +1,3 @@
+print(' I\'m Shubham Kumar ')
+print('Be it \\ dfg')
+print("git bash \"")
---
+++ class11-comments in python-CipherSchools.py
@@ -0,0 +1,3 @@
+print(' I\'m Shubham Kumar ') # \' is for '
+print('Be it \\ dfg') # \\ is for \
+print("git bash \"") # \" to print "
